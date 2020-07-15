06d875f36a9151195e9779e8c6566ca02592f091
hammadasad22 <hammadasad190@gmail.com>
2020-07-15 22:39:04 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
 
         <Route path="/" element={<Home />} /> 
         <Route path="contact" element={<Contact />} />
-        <Route path="about" element={<About />} />     
+        {/* <Route path="about" element={<About />} />      */}
 
 
       </Routes>
